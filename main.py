e33d37ba15eb181a63f5a23842d9b7e4f3b0910b
Dylan Buel <dylan@buel.me>
2025-03-04 10:49:10 -0700

    Changed the way that Discord webhooks were being handled and added MarkDown to output

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import asyncio
 import websockets
 import json
+import time
 from datetime import datetime, timezone
 from discord_webhook import DiscordWebhook
 
@@ -25,13 +26,18 @@ def getconfig():
     with open("config.json", 'r') as file:
         return json.load(file)
 
+def getdiscordtimestamp():
+    return f"<t:{round(time.time())}:f>"
+
 async def connect_ais_stream(config):
 
     print("Setting up Discord webhooks")
-    pos_discord_webhook = DiscordWebhook(url=config["WEB_HOOK"], content="Waiting for PositionReport")
-    pos_discord_webhook.execute()
-    static_discord_webhook = DiscordWebhook(url=config["WEB_HOOK"], content="Waiting for ShipStaticData")
-    static_discord_webhook.execute()
+    startup_message = "# Where is Capy \n"
+    pos_printtext = "Waiting for PositionReport"
+    static_printtext = "Waiting for ShipStaticData"
+    text_print = startup_message + pos_printtext + "\n" + static_printtext
+    discord_webhook = DiscordWebhook(url=config["WEB_HOOK"], content=text_print)
+    discord_webhook.execute()
 
     async with websockets.connect("wss://stream.aisstream.io/v0/stream") as websocket:
         subscribe_message = {"APIKey": config["API_KEY"],  # Required !
@@ -40,6 +46,7 @@ async def connect_ais_stream(config):
                             }
 
         subscribe_message_json = json.dumps(subscribe_message)
+        print("Waiting for message")
         await websocket.send(subscribe_message_json)
 
         async for message_json in websocket:
@@ -49,19 +56,20 @@ async def connect_ais_stream(config):
             if message_type == "PositionReport":
                 # the message parameter contains a key of the message type which contains the message itself
                 ais_message = message['Message']['PositionReport']
-                pos_printtext = f"[{datetime.now(timezone.utc)}] ShipId: {ais_message['UserID']} Status: {AIS_NAV_STATUS[ais_message['NavigationalStatus']]} Latitude: {ais_message['Latitude']} Latitude: {ais_message['Longitude']}"                 
-                pos_discord_webhook.content = pos_printtext
-                pos_discord_webhook.edit()
+                pos_printtext = f"{getdiscordtimestamp()} **ShipId:** {ais_message['UserID']} **Status:** {AIS_NAV_STATUS[ais_message['NavigationalStatus']]} **Latitude:** {ais_message['Latitude']} **Latitude:** {ais_message['Longitude']}"
                 print(pos_printtext)
+                pos_printtext = pos_printtext + "\n" + f"https://www.google.com/maps/place/@{ais_message['Latitude']},{ais_message['Longitude']},12.96z"
             elif message_type == "ShipStaticData":
                 ais_message = message['Message']['ShipStaticData']
-                static_printtext = f"[{datetime.now(timezone.utc)}] Name: {ais_message['Name']} Destination: {ais_message['Destination']} ETA: Day:{ais_message['Eta']['Day']} Hour:{ais_message['Eta']['Hour']} Minute:{ais_message['Eta']['Minute']}"
-                static_discord_webhook.content = static_printtext
-                static_discord_webhook.edit()
+                static_printtext = f"{getdiscordtimestamp()} **Name:** {ais_message['Name']} **Destination:** {ais_message['Destination']} **ETA:** Day:{ais_message['Eta']['Day']} Hour:{ais_message['Eta']['Hour']} Minute:{ais_message['Eta']['Minute']}"
                 print(static_printtext)
             else:
                 other_printtext = message
                 print(other_printtext)
+            
+            text_print = startup_message + pos_printtext + "\n" + static_printtext
+            discord_webhook.content = text_print
+            discord_webhook.edit()
 
 
 
